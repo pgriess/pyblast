0380c6a88b3413e8cfef561f08b24764df2b7589
Peter Griess <pg@std.in>
2014-05-22 14:12:18 -0500

    Add distutils support
    - Add simple setup.py
    - Move blast.py around to accommodate convention for distutils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyBlast',
+    version='0.1',
+    description='Easy-to-use wrapper for NCBI BLAST',
+    author='Peter Griess',
+    author_email='pg@std.in',
+    py_modules=['pyblast'])
